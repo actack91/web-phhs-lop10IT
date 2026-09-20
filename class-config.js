d1f5b0b4ee572cbb8681c2d9e1c4005a41fb899f
actack91 <quangdd.y14@gmail.com>
2026-09-20 10:54:24 +0700

    Connect 10IT website to its deployed Apps Script

--- class-config.js
+++ class-config.js
@@ -1,6 +1,6 @@
 window.CLASS_CONFIG = Object.freeze({
   "className": "LỚP 10IT",
   "academicYear": "2026 - 2027",
-  "apiUrl": "",
+  "apiUrl": "https://script.google.com/macros/s/AKfycbwK7wmyOEDU0a7_UADbI6XMVI7KtjlNGe1JjZ4H6zXyt0oxx_t8izOuerMKo9DJBZbPOg/exec",
   "spreadsheetUrl": "https://docs.google.com/spreadsheets/d/1gJNQ1OSCsaStvwC11MLoapPxwqNYcUdcgyYVy5qUhEo/edit"
 });
